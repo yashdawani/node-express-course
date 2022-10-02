ae9d124ce7eb31432663ce61e09848f2988378d9
Yash Dawani <yashdawani386@gmail.com>
2022-10-03 10:32:26 +1100

    Updated changes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,14 @@
-console.log('Express Tutorial')
+// const express = require("express");
+// const app = express();
+const app = require("express")();
+
+// home page
+app.get("/", (req, res) => {
+  res.type("text/plain");
+  res.send("home page");
+});
+
+// Deploying the server on the port 5100
+app.listen(5100, () => {
+  console.log("server is listening on port 5100...");
+});
